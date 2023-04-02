fb27e59123d2b9310d4bcfe4521b0e40669cba2a
StagoAce <santicastanoacevedo@gmail.com>
2023-04-02 11:05:25 -0500

    desde 0 security

--- spring-boot-data-jpa/src/main/java/com/base/datos/springbootdatajpa/Models/Entity/Usuario.java
+++ spring-boot-data-jpa/src/main/java/com/base/datos/springbootdatajpa/Models/Entity/Usuario.java
@@ -1,11 +1,17 @@
 package com.base.datos.springbootdatajpa.Models.Entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,12 +26,32 @@ public class Usuario implements Serializable{
 
     private String Password;
 
+    private boolean enabled;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private List<Rol> roles;
+
+    public Usuario() {
+        super();
+        roles = new ArrayList<Rol>();
+        enabled = true;
+    }
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -36,13 +62,25 @@ public class Usuario implements Serializable{
         Password = password;
     }
 
-    public String getUsername() {
-        return username;
+    public boolean isEnabled() {
+        return enabled;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
+
+    public List<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Rol> roles) {
+        this.roles = roles;
+    }
+
+    
+
+    
 
     
 }
